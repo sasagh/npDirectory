d5d941e678e506c1d54ca34bf85bddff94b0ca0a
sasagh <sa.saghinadze@gmail.com>
2021-08-30 20:48:49 +0400

    refactor naturalPersons controller

--- controllers/naturalPersons.js
+++ controllers/naturalPersons.js
@@ -1,26 +1,51 @@
 const NaturalPerson = require('../models/NaturalPerson')
 
-exports.getNaturalPersons = (req, res, next) => {
-    res.status(200).json({"success" : true, 'method' : 'get'});
+exports.getNaturalPersons = async (req, res, next) => {
+    const naturalPersons = await NaturalPerson.find();
+
+    if(naturalPersons.length > 0){
+        res.status(200).json({ success: true, size: naturalPersons.length, data: naturalPersons });
+    } else {
+        res.status(404).json({ success: false });
+    }
 }
 
 exports.createNaturalPerson = async (req, res, next) => {
     const naturalPerson = await NaturalPerson.create(req.body);
 
-    res.status(201).json({
-        data: naturalPerson
+    res.status(201).json({ data: naturalPerson });
+}
+
+exports.getNaturalPersonById = async (req, res, next) => {
+    const naturalPerson = await NaturalPerson.findById(req.params.id);
+
+    if(naturalPerson){
+        res.status(200).json({ success: true, data: naturalPerson });
+    } else {
+        res.status(404).json({ success: false });
+    }
+}
+
+exports.updateNaturalPerson = async (req, res, next) => {
+    const naturalPerson = await NaturalPerson.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
     });
+
+    if(naturalPerson){
+        res.status(200).json({ success: true, data: naturalPerson });
+    } else {
+        res.status(404).json({ success: false });
+    }
 }
 
-exports.getNaturalPersonById = (req, res, next) => {
-    res.status(200).json({'success' : true, 'method' : 'get:id'});
-}
+exports.deleteNaturalPerson = async (req, res, next) => {
+    const naturalPerson = await NaturalPerson.findByIdAndDelete(req.params.id);
 
-exports.updateNaturalPerson = (req, res, next) => {
-    res.status(200).json({'success' : true, 'method' : 'put:id'});
-}
-
-exports.deleteNaturalPerson = (req, res, next) => {
-    res.status(200).json({'success' : true, 'method' : 'delete:id'});
+    if(naturalPerson){
+        res.status(200).json({ success: true, data: {} });
+    } else {
+        res.status(404).json({ success: false });
+    }
 }
 
